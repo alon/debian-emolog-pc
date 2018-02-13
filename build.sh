fa2799123d35ee0fe862dfc05ec3f753bcce9d76
Alon Levy <alon@pobox.com>
2018-02-13 23:42:04 +0200

    variable version

--- build.sh
+++ build.sh
@@ -5,13 +5,15 @@
 #python setup.py sdist
 
 #Then here:
-SRC=emolog-0.1.tar.gz
+VERSION=$(dpkg-parsechangelog | grep Version | sed -e 's/Version: //' | sed -e 's/-.*//')
+NAME=emolog
+SRC=$NAME-$VERSION.tar.gz
 if [ ! -e $SRC ]; then
     echo "cd _emolog_; python setup.py sdist"
     echo "set SRC to the tarball"
     exit -1
 fi
 tar xvzf $SRC
-cd emolog-0.1
+cd $NAME-$VERSION
 ln -sf ../debian .
 dpkg-buildpackage -sa
